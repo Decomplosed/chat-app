7a7462ba6998c8d5a7347d04b5fbe79e5205207d
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-30 20:43:10 +0200

    created a server variable which is a new instance of http.createServer

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const http = require('http')
 const path = require('path')
 
 const app = express()
+const server = http.createServer(app)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = 3000 || process.env.PORT
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
