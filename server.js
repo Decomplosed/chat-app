22285cad4b48e07caa4ab423c027d4a40d460645
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-30 20:44:10 +0200

    initialised socket server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
