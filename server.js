57b9996356601c7380e9036f6b6e0d6c276fd551
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-30 21:10:08 +0200

    imported formatMessage

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
