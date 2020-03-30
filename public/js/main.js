4a70b52d30e06e1c6ea782cabb3e50d2b3cec406
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-30 21:01:38 +0200

    finished creating outputMessage function

--- public/js/main.js
+++ public/js/main.js
@@ -16,5 +16,13 @@ chatForm.addEventListener('submit', e => {
 
 function outputMessage(message) {
   const div = document.createElement('div')
+  div.classlist.add('message')
+  div.innerHTML = `
+    <p class='meta>Brad <span>9:12pm</span></p>
+    <p class='text>
+      ${message}
+    </p>
+    `
 
+  document.querySelector('.chat-messages').appendChild(div)
 }
--- server.js
+++ server.js
@@ -28,16 +28,3 @@ const PORT = 3000 || process.env.PORT
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-function outputMessage(message) {
-  const div = document.createElement('div')
-  div.classList.add('message')
-  div.innerHTML = `
-    <p class='meta>Brad <span>9:12pm</span></p>
-    <p class='text>
-      ${message}
-    </p>
-    `
-
-  document.querySelector('.chat-messages').appendChild(div)
-}
