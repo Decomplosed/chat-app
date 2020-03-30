3742d6f0d85d0707704df9f2fd99bff7cfe6932c
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-30 21:02:46 +0200

    fixed typo

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,7 @@ chatForm.addEventListener('submit', e => {
 
 function outputMessage(message) {
   const div = document.createElement('div')
-  div.classlist.add('message')
+  div.classList.add('message')
   div.innerHTML = `
     <p class='meta>Brad <span>9:12pm</span></p>
     <p class='text>
